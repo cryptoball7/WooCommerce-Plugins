ef3e3c436865b504fea732c7ac415826372abf6b
unknown <cryptoball7@gmail.com>
2025-12-17 19:19:02 -0800

    Creating React element.

--- agentic-payments/assets/js/blocks-payment.js
+++ agentic-payments/assets/js/blocks-payment.js
@@ -11,17 +11,30 @@
     const { registerPaymentMethod } = window.wc.wcBlocksRegistry;
     const { createElement } = window.wp.element;
 
-    console.log('[AgenticPayments][Blocks] JS loaded');
+    console.log('[AgenticPayments][Blocks] JS loaded');    console.log("hi");
+
+    const Content = createElement(
+        'div',
+        null,
+        'Pay via Agentic'
+    );
+
+    console.log("hi");
+    console.log(Content);
+
+    const Edit = createElement(
+        'div',
+        null,
+        'Agentic payment method'
+    );
 
     registerPaymentMethod({
         name: 'agentic',
         label: 'Agentic (programmatic)',
         ariaLabel: 'Pay with Agentic',
         canMakePayment: () => true,
-        content: () =>
-            createElement('div', null, 'Pay via Agentic'),
-        edit: () =>
-            createElement('div', null, 'Agentic payment method'),
+        content: Content,
+        edit: Edit,
     });
 
     console.log('[AgenticPayments][Blocks] registerPaymentMethod executed');
