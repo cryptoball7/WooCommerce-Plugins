ba85b01695f2754ea24c6bf06c259141db6f097b
unknown <cryptoball7@gmail.com>
2026-01-12 08:01:05 -0800

    Enabled WooCommerce in tests and ensured that WooCommerce is active

--- agentic-payments/tests/bootstrap.php
+++ agentic-payments/tests/bootstrap.php
@@ -36,3 +36,7 @@ tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Start up the WP testing environment.
 require "{$_tests_dir}/includes/bootstrap.php";
+
+require_once WP_PLUGIN_DIR . '/woocommerce/woocommerce.php';
+activate_plugin( 'woocommerce/woocommerce.php' );
+
